489e0da4edfc8e042f31e11188cba737c6bb85f1
saraMoAlhawamdeh <sarahmotasmomar@gmail.com>
2025-12-30 21:31:25 +0300

    Creating constants file

---
+++ src/main/java/org/example/Constants/DronsConstants.java
@@ -0,0 +1,10 @@
+package main.java.org.example.Constants;
+
+public class DronsConstants {
+    public static final String REGISTER = "REGISTER";
+    public static final String REQUEST_TASK = "REQUEST_TASK";
+    public static final String SUBMIT_RESULT =  "SUBMIT_RESULT";
+    public static final String HEARTBEAT =  "HEARTBEAT";
+    public static final String ALIVE =  "ALIVE";
+    public static final String LOST =  "LOST";
+}
