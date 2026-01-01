c9ec009a7717288424fd66ea53bdac89244f698e
reembirawi <reembirawi@gmail.com>
2026-01-01 17:33:41 +0300

    Add heartbeat.buffer.size to config.properties

--- src/main/java/org/example/config/ConfigKeys.java
+++ src/main/java/org/example/config/ConfigKeys.java
@@ -3,5 +3,6 @@ package org.example.config;
 public final class ConfigKeys {
     public static final String SERVER_PORT = "server.port";
     public static final String HEARTBEAT_TIMEOUT_SECONDS = "heartbeat.timeout.seconds";
+    public static final String HEARTBEAT_BUFFER_SIZE = "heartbeat.buffer.size";
 }
 
