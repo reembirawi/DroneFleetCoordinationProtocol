a25aac650200f8e90676a4c0a3bc99f0116f588b
reembirawi <reembirawi@gmail.com>
2026-01-02 18:13:38 +0300

    Add equals and hashCode overrides to GeoLocation class

--- src/main/java/org/example/util/GeoLocation.java
+++ src/main/java/org/example/util/GeoLocation.java
@@ -20,4 +20,23 @@ public class GeoLocation  implements Serializable {
     public double getLongitude() {
         return longitude;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GeoLocation)) return false;
+        GeoLocation other = (GeoLocation) o;
+        return Double.compare(latitude, other.latitude) == 0 &&
+                Double.compare(longitude, other.longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(latitude) * 31 + Double.hashCode(longitude);
+    }
+
+    @Override
+    public String toString() {
+        return "GeoLocation{" + "lat=" + latitude + ", lon=" + longitude + '}';
+    }
 }
