22c6fdbd2f08e520b357fd7e744c68bdc7578cfe
reembirawi <reembirawi@gmail.com>
2026-01-02 11:38:20 +0300

    add TaskConstants class

---
+++ src/main/java/org/example/Constants/TaskConstants.java
@@ -0,0 +1,7 @@
+package org.example.Constants;
+
+public class TaskConstants {
+    public static final String PENDING = "PENDING";
+    public static final String IN_PROGRESS = "IN_PROGRESS";
+    public static final String COMPLETED =  "COMPLETED";
+}
